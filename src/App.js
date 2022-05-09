9da8797a80e3399c231b31d47c5060c79d08ce63
RafaRed <rafaelszcardoso@gmail.com>
2022-05-09 17:23:12 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Navbar from './components/js/Navbar';
+
+function App() {
+  return (
+   <Navbar></Navbar>
+  );
+}
+
+export default App;
---
+++ src/components/js/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Navbar.css";
+function Navbar() {
+	return (
+		<div className="navbar">
+			<div className="logo">
+				<img className="logoimg" src="images/logo.png"></img>
+				<p>LINEUPS</p>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
